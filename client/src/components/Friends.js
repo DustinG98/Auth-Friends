542912f5439c835afbcea6d34177ae49d4e14d03
DustinG98 <thedustingraham@gmail.com>
2020-02-14 19:23:04 -0700

    Base url includes api, friends makes get request on initial render

--- client/src/components/Friends.js
+++ client/src/components/Friends.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
 
 const Friends = () => {
+    const [friends, setFriends] = useState([])
+    useEffect(() => { 
+        axiosWithAuth().get('/friends')
+            .then(res => setFriends(res.data))
+            .catch(err => console.log(err))
+    }, [])
     return (
         <div>
             <h2>Friends</h2>
+            {friends.map(friend => {
+                return <div key={friend.id}>
+                    <h3>{friend.name}</h3>
+                    <h5>{friend.age}</h5>
+                    <h6>{friend.email}</h6>
+                </div>
+            })}
         </div>
     )
 }
